83969167b41b3de14272d07d5ea7574dc82c3ead
Mian King <bilal.jan085@gmail.com>
2021-08-09 23:59:55 +0200

    Remove dummy expense data and deleted yarnerror log file

--- src/app.js
+++ src/app.js
@@ -11,17 +11,18 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
-
-const state = store.getState();
-const visibleExpenses =  getVisibleExpenses(state.expenses, state.filters);
-console.log(store.getState());
 
 const jsx = (
     <Provider store={store}>
-        <AppRouter />
+    <AppRouter />
     </Provider>
-)
-ReactDOM.render(jsx, document.getElementById('app'));
+    )
+    ReactDOM.render(jsx, document.getElementById('app'));
+    
+    // store.dispatch(addExpense({ description: 'Water bill', amount: 4500}));
+    // store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000}));
+    // store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
+    
+    // const state = store.getState();
+    // const visibleExpenses =  getVisibleExpenses(state.expenses, state.filters);
+    // console.log(store.getState());
