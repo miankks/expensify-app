bc088b31efe14fe7f295a803274bff7b1fe0999d
Mian King <bilal.jan085@gmail.com>
2021-08-11 02:07:18 +0200

    css and sass loader added and Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-
 const jsx = (
     <Provider store={store}>
     <AppRouter />
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,59 @@
 // entry point - output
 
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // console.log(path.join(__dirname, 'public'));
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        // 'css-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                        // 'sass-loader'
+                    ]
+                })
+            }]
+        },
+        plugins : [
+            CSSExtract
+        ],
+        devtool: isProduction? 'source-map' : 'inline-source-map',
+        // devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-}
+};
+
+// [
+//     'style-loader',
+//     'css-loader',
+//     'sass-loader'
+// ]
