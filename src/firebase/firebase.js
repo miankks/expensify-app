65f3c751bfa47e93bf1f7317d3c636414b6bf3d1
Mian King <bilal.jan085@gmail.com>
2021-08-23 01:10:54 +0200

    comments removed in frebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,12 +9,6 @@ const firebaseConfig = {
     messagingSenderId: "253864619648",
     appId: "1:253864619648:web:cdf40da2e5fb28adc14065",
     measurementId: "G-JH148VDTZE"
-  // apiKey: process.env.FIREBASE_API_KEY,
-  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.FIREBASE_DATABASE_URL,
-  // projectId: process.env.FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(firebaseConfig);
