dd02f77ad814f2e9f7ff2138cd8f87620296b84c
Mian King <bilal.jan085@gmail.com>
2021-08-22 20:33:40 +0200

    Add styles and add babel-polyfill

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/dashboard" activeClassName="is-active">Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <button onClick= {startLogout }>Logout</button>
+  <header className="header">
+    <div className="content-container">
+      <div className="header__content">
+        <Link className="header__title" to="/dashboard">
+          <h1>Expensify</h1>
+        </Link>
+        <button className="button button--link" onClick= {startLogout }>Logout</button>
+      </div>
+    </div>
   </header>
 );
 
@@ -16,3 +20,8 @@ const mapDispatchToProps = (dispatch) => ({
   startLogout: () => dispatch(startLogout())
 })
 export default connect(undefined, mapDispatchToProps)(Header);
+
+// <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+// <NavLink to="/dashboard" activeClassName="is-active">
+// <h1>Expensify</h1>
+// </NavLink>
